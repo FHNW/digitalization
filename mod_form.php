e5b281c0c6833a98363a7770123d6d3bf7f9127f
Dimitri Vorona <vorona@in.tum.de>
2018-06-03 15:05:26 +0200

    Remove help button from library select

--- mod_form.php
+++ mod_form.php
@@ -117,7 +117,6 @@ class mod_digitalization_mod_form extends moodleform_mod
             // library
             $library = $mform->addElement('select', 'library', get_string('libraries_select', 'digitalization'), get_libraries());
             $mform->addRule('library', null, 'required', null, 'client');
-            $mform->addHelpButton('library', 'libraries_select', 'digitalization');
             if (isset($_SESSION['dig_library']) && ($_SESSION['dig_library'] != null)) {
                 $mform->setDefault('library', deserialize_library($_SESSION['dig_library']));
             }
