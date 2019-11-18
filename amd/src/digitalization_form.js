f4c4def358f36a1fb71b940855c9da70a961dbf4
Thomas Schallert <thomas.schallert@gmail.com>
2019-11-18 17:12:45 +0100

    Update digitalization_form.js

--- amd/src/digitalization_form.js
+++ amd/src/digitalization_form.js
@@ -1,43 +1,77 @@
-define(['core/str', 'jquery', 'theme_bootstrapbase/bootstrap'], function(str, $) {
-    // var tooltip = $.fn.tooltip.noConflict();
-    var NEBIS_URL = 'http://recherche.nebis.ch/';
-    var module = {
-        init: function() {
-            // add direct link to the library
-            $('#id_library_url').before(
-                $('<a />')
-                    .attr('id', 'library_search_link')
-                    .attr('href', NEBIS_URL)
-                    .attr('target', '_blank')
-            );
-
-            $('#id_back_to_automatic').click(function() {
-                // prevent validation erros
-                $('#id_author, #id_atitle, #id_title, #id_pub_date, #id_pages').each(function() {
-                    if ($(this).val() === '') {
-                        $(this).val(' ');
-                    }
-                });
-            });
-
-            var warningPresent = str.get_string('warning_submit_order', 'digitalization');
-            $.when(warningPresent).done(function(warningString) {
-                    $('#id_submitbutton2')
-                        .attr('data-toggle', 'tooltip')
-                        .attr('data-placement', 'top')
-                        .attr('title', warningString)
-                        .tooltip();
-            });
-
-            var helpPresent = str.get_string('to_library_catalogue', 'digitalization');
-            $.when(helpPresent).done(function(helpString) {
-                    $('#library_search_link')
-                        .attr('data-toggle', 'tooltip')
-                        .attr('data-placement', 'right')
-                        .attr('title', helpString)
-                        .tooltip();
-            });
+define(["core/str", "jquery", "theme_bootstrapbase/bootstrap"], function(
+  str,
+  $
+) {
+  // var tooltip = $.fn.tooltip.noConflict();
+  var NEBIS_URL = "http://recherche.nebis.ch/";
+  var SWISSBIB_URL = "https://baselbern.swissbib.ch/";
+  var module = {
+    init: function() {
+      // customize link
+      $("#id_library").change(function() {
+        var value = $(this).val();
+        if (value == 12) {
+          $("#library_search_link").remove();
+          $("#id_library_url").before(
+            $("<a />")
+              .attr("id", "library_search_link")
+              .attr("href", SWISSBIB_URL)
+              .attr("target", "_blank")
+          );
+        } else {
+          $("#library_search_link").remove();
+          $("#id_library_url").before(
+            $("<a />")
+              .attr("id", "library_search_link")
+              .attr("href", NEBIS_URL)
+              .attr("target", "_blank")
+          );
         }
-    };
-    return module;
+      });
+
+      // add direct link to the library
+      $("#id_library_url").before(
+        $("<a />")
+          .attr("id", "library_search_link")
+          .attr("href", NEBIS_URL)
+          .attr("target", "_blank")
+      );
+
+      $("#id_back_to_automatic").click(function() {
+        // prevent validation erros
+        $("#id_author, #id_atitle, #id_title, #id_pub_date, #id_pages").each(
+          function() {
+            if ($(this).val() === "") {
+              $(this).val(" ");
+            }
+          }
+        );
+      });
+
+      var warningPresent = str.get_string(
+        "warning_submit_order",
+        "digitalization"
+      );
+      $.when(warningPresent).done(function(warningString) {
+        $("#id_submitbutton2")
+          .attr("data-toggle", "tooltip")
+          .attr("data-placement", "top")
+          .attr("title", warningString)
+          .tooltip();
+      });
+
+      var helpPresent = str.get_string(
+        "to_library_catalogue",
+        "digitalization"
+      );
+      $.when(helpPresent).done(function(helpString) {
+        $("#library_search_link")
+          .attr("data-toggle", "tooltip")
+          .attr("data-placement", "right")
+          .attr("title", helpString)
+          .tooltip();
+      });
+    }
+  };
+  return module;
 });
